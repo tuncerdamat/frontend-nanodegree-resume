a754d62c08342a8f9dacf905a428ac1af486a546
Tuncer Damat <tuncer.damat@gmail.com>
2019-12-11 19:03:30 +0100

    print work history to the page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -18,24 +18,28 @@ var bio = {
 var work = {
     "jobs": [
         {
+            "startDate": "Feb 2014",
             "employer": "Vizyon Komünikasyon Teknolojileri",
             "title": "Junior Web Developer",
             "dates": "Feb 2014 - December 2015",
             "description": "Support team"
         },
         {
+            "startDate": "Jun 2016",
             "employer": "Facile.it",
             "title": "Web Developer",
             "dates": "Jun 2016 - May 2018",
             "description": "Development"
         },
         {
+            "startDate": "Jun 2018",
             "employer": "Wellnet S.r.l",
             "title": "Web Developer",
             "dates": "Jun 2018 - Jun 2019",
             "description": "Development"
         },
         {
+            "startDate": "Jul 2019",
             "employer": "Lenis S.r.l",
             "title": "Web Developer",
             "dates": "Jul 2019 - Current",
@@ -94,4 +98,12 @@ $('#main').append(bio.pictureURL);
 $('#main').append(work.currentJob);
 $('#main').append(education['nameOfTheLastSchoolAttended']);
 
+work.jobs.forEach(function (jobObject) {
+    $('#workExperience').append(HTMLworkStart);
 
+    $('.work-entry:last').append(
+        HTMLworkEmployer.replace('%data%', jobObject.employer)
+            + HTMLworkTitle.replace('%data%', jobObject.title));
+    $('.work-entry:last').append(HTMLworkDates.replace('%data%', jobObject.dates));
+    $('.work-entry:last').append(HTMLworkDescription.replace('%data%', jobObject.description));
+});
